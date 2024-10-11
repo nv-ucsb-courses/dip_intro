5ba5c7e2822db6b5bc3914890d4e66efad65df7e
Anantajit Subrahmanya <anantajit@ucsb.edu>
2024-10-10 18:18:02 -0700

    Add hw2 template

---
+++ homework/HW2/hw2.py
@@ -0,0 +1,34 @@
+# The following is an optional template to be used for the Homework 2 programming assignment
+
+from PIL import Image
+
+def Flip(A: Image) -> Image:
+    # your code here
+    pass
+
+def Flop(A: Image) -> Image:
+    # your code here
+    pass
+
+def Inv(A: Image) -> Image:
+    # your code here
+    pass
+
+def InvAlpha(A: Image) -> Image:
+    # your code here
+    pass
+
+def Over(A: Image, B: Image) -> Image:
+    # your code here
+    pass
+
+def Atop(A: Image, B: Image) -> Image:
+    # your code here
+    pass
+
+def Xor(A: Image, B: Image) -> Image:
+    # your code here
+    pass
+
+if __name__ == "__main__":
+    # Write test code here
